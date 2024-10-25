b87a32e7684937ab0aad32df1b5930045a149137
nur anisa <nrnsa4560@gmail.com>
2024-10-25 10:14:57 +0700

    memperbaiki tampilan edit produk

--- resources/views/livewire/edit-product.blade.php
+++ resources/views/livewire/edit-product.blade.php
@@ -129,7 +129,7 @@
                     <!-- End Col -->
                 </div>
                 <!-- End Section -->
-                <button wire:submit="update" enctype="multipart/form-data" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                <button wire:submit="update" enctype="multipart/form-data" type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                     <div wire:loading class="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-white-600 rounded-full" role="status" aria-label="loading">
                       <span class="sr-only">Loading...</span>
                     </div>  
