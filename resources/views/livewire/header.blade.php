5a97ae21c0a1444827017acb02bbc8fb7a7326c1
nur anisa <nrnsa4560@gmail.com>
2024-10-29 08:41:25 +0700

    memperbaiki header

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -46,7 +46,7 @@
           @else
             <a
               class="block rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700"
-              href="/login"
+              href="/register"
             >
               Get Started
             </a>
